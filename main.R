1ec07c63b1853467af060e1fc85d20e9fda0aaca
André Calero Valdez <andrecalerovaldez@gmail.com>
2024-06-05 09:47:46 +0200

    added cleaning code

--- main.R
+++ main.R
@@ -4,3 +4,33 @@ source("raw/survey_456893_R_syntax_file.R")
 
 raw_data <- data
 rm(data)
+
+
+library(tidyverse)
+library(labelled)
+
+
+raw_data <- as_tibble(raw_data)
+named_data <- set_variable_labels(.data = raw_data, .labels = attributes(raw_data)$variable.labels)
+
+# value labels do not make sense for Lime Survey data, as all factors already have levels and labels
+#
+#var_names <- names(named_data)
+#for (var_name in var_names) {
+#  value_lab <- attributes(named_data %>% pull(var_name))$levels
+#  val_labels(named_data[[var_name]]) <- value_lab
+#}
+
+
+
+clean_data <- named_data %>%
+  # remove unfinished respondents
+  filter(lastpage == 4) %>%
+  # remove columns that are not needed or do not contain any data
+  select(-submitdate, -startlanguage, -studyProgram_other)
+
+
+
+labelled::generate_dictionary(clean_data) %>% write_csv("data/data_dictionary.csv")
+
+
